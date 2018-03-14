fa05cd6b5dc67677b73c3449bffa44000e8bf7e1
Kristin Baumann <mail@kristin-baumann.com>
2018-03-14 11:34:17 +0100

    Run vis on first load

--- src/index.js
+++ src/index.js
@@ -5,44 +5,59 @@ import { calculateScales } from "./scales";
 import { configureChart, drawPath } from "./chart";
 import "./style.scss";
 
-import legendByDigit from './images/legendByDigit.png';
-import legendByRange from './images/legendByRange.png';
+import legendByDigit from "./images/legendByDigit.png";
+import legendByRange from "./images/legendByRange.png";
 
+const cleanVis = () => {
+  select("#chart svg").remove();
+};
 
-document.querySelector(".startButton").onclick = () => {
-    // get settings
-    var digits = Number(document.querySelector('#setting-digits').value);
-    var colorMode = document.querySelector('#setting-colormode').value;
-    var animation = document.querySelector('#setting-animation').value;
+const runVis = () => {
+  // get settings
+  var digits = Number(document.querySelector("#setting-digits").value);
+  var colorMode = document.querySelector("#setting-colormode").value;
+  var animation = document.querySelector("#setting-animation").value;
 
-    // call drawing function
-    startVis(digits, colorMode, animation);
+  // call drawing function
+  startVis(digits, colorMode, animation);
 
-    // show legend
-    const legend = document.querySelector('.legend')
-    legend.src = colorMode === 'colorByDigit' ?  legendByDigit : legendByRange;
-    legend.classList.remove("legendByDigit","legendByRange");
-    legend.classList.add(colorMode === 'colorByDigit' ?  "legendByDigit" : "legendByRange");
-
-}
+  // show legend
+  const legend = document.querySelector(".legend");
+  legend.src = colorMode === "colorByDigit" ? legendByDigit : legendByRange;
+  legend.classList.remove("legendByDigit", "legendByRange");
+  legend.classList.add(
+    colorMode === "colorByDigit" ? "legendByDigit" : "legendByRange"
+  );
+};
 
 const startVis = (digits, colorMode, velocity) => {
-    // remove previous data vis
-    select("#chart svg").remove();
+  // remove previous data vis
+  cleanVis();
 
-    // get and clean data
-    calculatePathElements(digits, step, startPoint, (data) => {
+  // get and clean data
+  calculatePathElements(digits, step, startPoint, data => {
+    // create svg element
+    const svgElement = select("#chart").append("svg");
 
-        // create svg element
-        const svgElement = select("#chart").append("svg");
-        
-        // set dimensions of chart
-        configureChart(svgElement, margin, width, height);
-        
-        // calculate scales
-        const scales = calculateScales(data, width, height);
-        
-        // draw lines
-        drawPath(svgElement, data, scales, margin, colorMode, velocity, startPoint);
-    });
-}
+    // set dimensions of chart
+    configureChart(svgElement, margin, width, height);
+
+    // calculate scales
+    const scales = calculateScales(data, width, height);
+
+    // draw lines
+    drawPath(svgElement, data, scales, margin, colorMode, velocity, startPoint);
+  });
+};
+
+// run vis when clicking generate button
+document.querySelector(".startButton").onclick = () => runVis();
+
+// run vis on first load
+runVis();
+
+// remove previous data vis when settings change
+document.querySelector("#setting-digits").onchange = () => cleanVis();
+document.querySelector("#setting-colormode").onchange = () => cleanVis();
+document.querySelector("#setting-colormode").onchange = () => cleanVis();
+
